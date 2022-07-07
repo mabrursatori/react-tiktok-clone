94d88172ec98bd5595aea2a4ce7adeac25b84dd4
mabrursatori <mabrursatori@gmail.com>
2022-07-07 15:52:38 +0700

    add google auth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,10 +6,13 @@ import { AiOutlineLogout } from 'react-icons/ai';
 import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
 import { GoogleLogin, googleLogout  } from '@react-oauth/google';
+import useAuthStore from '../store/authStore';
 
 import Logo from '../utils/tiktik-logo.png';
+import { createOrGetUser } from '../utils';
 
 const Navbar = () => {
+  const {userProfile, addUser, removeUser} = useAuthStore();
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
       <Link href="/">
@@ -22,6 +25,50 @@ const Navbar = () => {
           />
         </div>
       </Link>
+
+      <div>SEARCH</div>
+
+     
+      <div>
+        {userProfile ? (
+          <div className='flex gap-5 md:gap-10'>
+            <Link href='/upload'>
+              <button className='border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2'>
+                <IoMdAdd className='text-xl' />{' '}
+                <span className='hidden md:block'>Upload </span>
+              </button>
+            </Link>
+            {userProfile.image && (
+              <Link href={`/profile/${userProfile._id}`}>
+                <div>
+                  <Image
+                    className='rounded-full cursor-pointer'
+                    src={userProfile.image}
+                    alt='user'
+                    width={40}
+                    height={40}
+                  />
+                </div>
+              </Link>
+            )}
+              <button
+                type='button'
+                className=' border-2 p-2 rounded-full cursor-pointer outline-none shadow-md'
+                onClick={() => {
+                  googleLogout();
+                   removeUser();
+                }}
+              >
+                <AiOutlineLogout color='red' fontSize={21} />
+              </button>
+          </div>
+        )  : (
+        <GoogleLogin
+        onSuccess={(response) => createOrGetUser(response, addUser)}
+        onError={() => console.log('error')}
+        />
+      )}
+      </div>
     </div>
   )
 }
